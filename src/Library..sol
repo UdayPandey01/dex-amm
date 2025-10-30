c1fdb3be027adb0e308e59f53037cf0a2a5cc821
Uday Pandey <pandeyuday113@gmail.com>
2025-10-30 08:39:25 +0000

    added different library for inner call function and implement swap functions

---
+++ src/Library..sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library Library {
+    function sortTokens(address tokenA, address tokenB)
+        internal
+        pure
+        returns (address token0, address token1)
+    {
+        require(tokenA != tokenB, "Identical addresses");
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), "Zero address");
+    }
+
+    function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
+        (address token0, address token1) = sortTokens(tokenA, tokenB);
+        pair =  address(uint(keccak256(abi.encodePacked(
+            hex"ff",
+            factory,
+            keccak256(abi.encodePacked(token0, token1)),
+            hex"96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f"
+        ))));
+    }
+
+    function getAmountOut(
+        uint256 amountIn,
+        uint256 reserveIn,
+        uint256 reserveOut
+    ) external pure returns (uint256 amountOut) {
+        uint256 amountInWithFee = amountIn * 997;
+        uint256 numerator = amountInWithFee * reserveOut;
+        uint256 denominator = (reserveIn * 1000) + amountInWithFee;
+        amountOut = numerator / denominator; 
+    }
+}
--- src/Router.sol
+++ src/Router.sol
@@ -1,51 +1,78 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./Pair.sol";
+import "./Library.sol";
+
 contract Router {
-    function swapExactTokensForTokens(
-        uint256 amountIN,
-        uint256 amountOutMin,
-        address[] calldata path,
-        address to,
-        uint256 deadline
-    ) external returns (uint256[] memory amounts) {
+    address public factory;
 
+    constructor(address _factory) {
+        factory = _factory;
     }
 
-    function sortTokens(address tokenA, address tokenB)
-        internal
-        pure
-        returns (address token0, address token1)
-    {
-        require(tokenA != tokenB, "Identical addresses");
-        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), "Zero address");
-    }
-
-    function getReserves(
+    function _getReserves(
         address factory,
         address tokenA,
         address tokenB
     ) external view returns (uint256 reserveA, uint256 reserveB) {
         (address token0,) = sortTokens(tokenA, tokenB);
+        (uint256 reserve0, uint256 reserve1) = Pair(Library.pairFor(factory, tokenA, tokenB)).getReserves();
+        (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
 
-    function getAmountOut(
-        uint256 amountIn,
-        uint256 reserveIn,
-        uint256 reserveOut
-    ) external pure returns (uint256 amountOut) {
-        uint256 amountInWithFee = amountIn * 997;
-        uint256 numerator = amountInWithFee * reserveOut;
-        uint256 denominator = (reserveIn * 1000) + amountInWithFee;
-        amountOut = numerator / denominator; 
-    }
+    // function _swap(uint[] memory amounts, address[] memory path, address _to) private {
+    //     for (uint i; i < path.length - 1; i++) {
+    //         (address input, address output) = (path[i], path[i + 1]);
+    //         (address token0,) = UniswapV2Library.sortTokens(input, output);
+    //         uint amountOut = amounts[i + 1];
+    //         (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
+    //         address to = i < path.length - 2 ? UniswapV2Library.pairFor(factory, output, path[i + 2]) : _to;
+    //         Pair(pairFor(factory, input, output)).swap(amount0Out, amount1Out, to, new bytes(0));
+    //     }
+    // }
 
     function getAmountsOut(
         address factory,
         uint256 amountIn,
         address[] calldata path
     ) external returns (uint256[] memory amounts) {
-
+        require(path.length >= 2, "Invalid path");
+        amounts = new uint256[](path.length);
+        amounts[0] = amountIn;
+        for(uint256 i; i < path.length - 1; i++) {
+            (uint256 reserveIn, uint256 reserveOut) = this.getReserves(factory, path[i], path[i + 1]);
+            amounts[i + 1] = this.getAmountOut(amounts[i], reserveIn, reserveOut);
+        }
     }
+
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts) {
+        amounts = getAmountsOut(amountIn, path);
+
+        require(amounts[amounts.length - 1] >= amountOutMin, "Insufficient output");
+
+        for (uint i; i < path.length - 1; i++) {
+            (address input, address output) = (path[i], path[i + 1]);
+            (uint amount0Out, uint amount1Out) = input < output
+                ? (uint(0), amounts[i + 1])
+                : (amounts[i + 1], uint(0));
+
+            address nextTo = i < path.length - 2
+                ? Library.pairFor(factory, output, path[i + 2])
+                : to;
+
+            Pair(Library.pairFor(factory, input, output)).swap(
+                amount0Out,
+                amount1Out,
+                nextTo
+            );
+        }
+    }
+
 }
